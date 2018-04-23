cfb5d1dc4de9b08f0e4227712c0e14b092d48b41
LoveqLRC <664215432@qq.com>
2018-04-23 15:29:27 +0800

    Initial commit

---
+++ app/src/main/java/com/loveqrc/constraintdemo/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.loveqrc.constraintdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.tv_one -> {
+                KeyFrameAnimationActivity.launchActivity(this)
+            }
+            R.id.tv_two -> {
+
+            }
+            R.id.tv_collapsing_toolbar -> {
+
+            }
+
+            R.id.tv_parallax_effect -> {
+
+            }
+        }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv_one.setOnClickListener(this)
+        tv_two.setOnClickListener(this)
+        tv_collapsing_toolbar.setOnClickListener(this)
+        tv_parallax_effect.setOnClickListener(this)
+    }
+}
